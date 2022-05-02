89da27cdcbcbcb4775829667b8f74ec42acc3c1a
Linda Strålberg <linda.stralberg@gameysgroup.com>
2022-05-02 20:58:40 +0200

    Import Roboto font

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,16 +1,6 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+        <h1>Card Game</h1>
     </div>
 </template>
 
@@ -21,3 +11,9 @@
         }
     }
 </script>
+
+<style scoped>
+h1 {
+    font-family: 'Roboto';
+}
+</style>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <script src="{{ mix('/js/app.js') }}" defer></script>
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
